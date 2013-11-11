e3ee5630f28911abdc7bf1f83bb69d2e40f7f46a
Siddharth Choudhary <siddharthchoudhary@gatech.edu>
2013-11-11 11:04:41 -0500

    Getopts Long use all bugs fixed...

--- w9p2.pl
+++ w9p2.pl
@@ -1,10 +1,9 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -w
 
 # Perl script to overlap two sets of genomic coordinates and finding intersecting members.
 
 use strict;
-use warnings;
-use Getopt::Std;
+use Getopt::Long;
 use List::Util qw[min max]; 
 
 my %args;
@@ -12,8 +11,8 @@ my @cols;
 my $joinFlag=0;
 my $str_cond='any';							#default condition is 'any'
 my $str_cond_flag = 0;
-my $file1="file1.bed";
-my $file2="file2.bed";
+my $file1;
+my $file2;
 my $per_over=100;							#default overlap percentage is 100
 my $tagf1;
 my $startf1=0;
@@ -25,51 +24,38 @@ my $endf2=0;
 my $strf2;
 my $linef1;
 my $linef2;
+my $linef2_temp;
 my $maxStart;
 my $minEnd;
+my $helpflag=0;
 
 sub helpprinter{
-	print "To run the scrpt: ./script_name <options> -o <overlap_percentage> -f1 <bed_file1> -f2 <bed_file2>\n";
-	print "Options are:\nh -- help\nj -- join\nc -- strand_condition{E.g.: -c eq or -c ne}\n";
-#	exit;
+	print "To run the scrpt: ./script_name <options> -o <overlap_percentage> --file1=<bed_file1> --file2=<bed_file2>\n";
+	print "Options are:\nh -- help\nj -- join\nc -- strand_condition{E.g.: --condition=eq or -condition=ne}\n";
+	exit;
 }
 
 &helpprinter() if($#ARGV == 0);
 
-getopts('hj:o:f1:f2:c:', \%args);
+&helpprinter() if (@ARGV < 1 or ! GetOptions(
+		"help" => \$helpflag,
+		"join" => \$joinFlag,
+		"overlap=i" => \$per_over,
+		"file1=s" => \$file1,
+		"file2=s" => \$file2,
+		"condition=s" => \$str_cond) or $helpflag == 1);
 
-print "To run the script: ./script_name <options> -f1 <bed_file1> -f2 <bed_file2>\n" if ($args{h});
-
-#Option to print overlapping member. i.e. result of joining the rows.
-$joinFlag = 1 if ($args{'j'});
-
-if (defined $args{'f1'}){
-	$file1 = $args{'f1'};
+if($str_cond eq 'eq' or $str_cond eq 'ne'){
+	$str_cond_flag = 1;
 }
 else{
-	&helpprinter();
-}
-
-if (defined $args{f2}){
-	$file2 = $args{f2};
-}
-else{
-	&helpprinter();
-}
-
-if (defined $args{o}){
-	$per_over = $args{o};
-}
-
-if (defined $args{c}){
-	$str_cond_flag=1;
-	$str_cond = $args{c};
+	&helpprinter() if ($str_cond ne 'any');
 }
 
 open(my $fh1, "<", $file1) or die "Can't open file: $file1\n";
 open(my $fh2, "<", $file2) or die "Can't open file: $file2\n";
 
-$linef2 = <$fh2>;
+exit if (!($linef2 = <$fh2>));
 chomp $linef2;
 @cols = split("\t", $linef2);
 $tagf2 = $cols[0];
@@ -102,8 +88,10 @@ while($linef1 = <$fh1>){
 	$strf1 = $cols[5];
 
 	while (1){
-		last if(($startf2 < $endf1) && ($tagf1 eq $tagf2));
-		$linef2 = <$fh2>;
+#		print "$startf2, $endf1, $endf2, $startf1, $tagf1, $tagf2\n";
+		last if(($startf2 < $endf1) && ($endf2 > $startf1) && ($tagf1 eq $tagf2));
+		last if (!($linef2_temp = <$fh2>));
+		$linef2 = $linef2_temp;
 		chomp $linef2;
 		@cols = split("\t", $linef2);
 		$tagf2 = $cols[0];
@@ -121,6 +109,9 @@ while($linef1 = <$fh1>){
 	}
 	$maxStart = max($startf1, $startf2);
 	$minEnd = min($endf1, $endf2);
+#	printf ("%f\n", (($minEnd - $maxStart)/($endf1 - $startf1) * 100));
+#	print "$minEnd, $maxStart, $endf1, $startf1, $endf2\n";
+#	print "$linef1\n";
 	if((($minEnd - $maxStart)/($endf1 - $startf1) * 100) >= $per_over){
 		if($joinFlag){
 			if($str_cond_flag)
